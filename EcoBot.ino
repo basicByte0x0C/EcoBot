cfaae7264904fbf7cf7fde03ee68dfa192cd0883
basicByte0x0C <>
2024-09-02 18:08:05 +0300

    Motor Phase: Motors are running!  - Added functions for Motor Handling  - Added some notes and documentation  - Tested both motors and they work as expected

--- EcoBot.ino
+++ EcoBot.ino
@@ -1,73 +1,70 @@
 /***************************************************************************************
  * Includes
  **************************************************************************************/
+#include "Notes.h"
 
 /***************************************************************************************
  * Macros
  **************************************************************************************/
-/* General stuff */
+/* General Stuff */
 #define E_OK            0u
 #define E_NOT_OK        1u
-#define DELAY_DEFAULT   1000
+#define DELAY_1_SECOND  1000
+#define DELAY_DEFAULT   DELAY_1_SECOND
+/* General Stuff end */
 
 /* Motor Stuff */
-#define PIN_DRV8834_M1      D4
-#define PIN_DRV8834_SLEEP   D6
-#define PIN_MA_PHASE        D7
-#define PIN_MA_ENABLE       D3
-#define PIN_MB_PHASE        D8
-#define PIN_MB_ENABLE       D5
+#define PIN_MA_ENABLE       3
+#define PIN_MA_PHASE        4
+#define PIN_MB_ENABLE       5
+#define PIN_MB_PHASE        6
+#define PIN_DRV8834_SLEEP   7
 
 #define DRV8834_MOTOR_A             1u
 #define DRV8834_MOTOR_B             2u
 #define DRV8834_MOTOR_BOTH          3u
 #define DRV8834_DIRECTION_BACKWARD  0u
 #define DRV8834_DIRECTION_FORWARD   1u
-
-/***************************************************************************************
- * Notes
- **************************************************************************************/
-
-/* MOTOR DRIVER :: Will use DRV8834 in Phase/Enable Mode with 2x 3-6v DC Motors */
-/* BATTERY :: Will use a LiPo battery, for now we'll try to use one old 14500 cell */
-
-/* TODO: Function to set Driver Max Current 
- * - My Motors seems to use 120mA max each */
-
-/* ----- How to controll 2 motors -----
- * - 2 DC Motors can be controlled by DRV8834 with Phase/Enable mode
- * - Each Motor has a ENABLE Pin and a PHASE Pin (xENBL and xPHASE)
- * - xPHASE is controlling the direction, while xENBL controlls the motor
- * - xENBL can be used as PWM to controll the motor
- * - M1 on Low + xENBL on low => Break Mode (0v on both + and -) */
-
-/* TODO: RotateRight(degree) and RotateLeft(degree) functions
- * - Which will move the motors accordingly so the robot can rotate x degrees to the right or left */
-
-/* TODO: Sleep like a baby
- * - Robot will sleep while charging and when the battery level is low
- * - Motor Driver put into sleep mode 
- * - Arduino will sleep and wake up from time to time to check Battery level */
+/* Motor Stuff end */
 
 /***************************************************************************************
  * Function: Motor_Break()
  ***************************************************************************************
- * Description: This can be called such that all motors will break and stop moving
+ * Description: This can be called such that a motor will break and stop moving.
+ * Parameters:
+ *  - motorIdentifier[in]   :   Identifier of the motor to be switched forward
+ *                              Supported Inputs: 
+ *                                  DRV8834_MOTOR_A == 1u
+ *                                  DRV8834_MOTOR_B == 2u
+ *                                  DRV8834_MOTOR_BOTH == 3u
  **************************************************************************************/
-void Motor_Break(void)
+void Motor_BreakMotor(byte motorIdentifier)
 {
     /* To make the motors break in Phase/Enable Mode you need to:
-        - M1 pin on Low
         - xENBL on Low
         - xPHASE doesn't matter
        The outputs will be both 0v => Motor will stop */
 
-    /* Disable M0 Pin */
-    digitalWrite(PIN_DRV8834_M1, LOW);
-
-    /* Disable Motors */
-    analogWrite(PIN_MA_ENABLE, LOW);
-    analogWrite(PIN_MB_ENABLE, LOW);
+    /* Check which motor to break */
+    switch(motorIdentifier)
+    {
+      case DRV8834_MOTOR_A:
+        /* Break Motor A */
+        analogWrite(PIN_MA_ENABLE, LOW);
+        break;
+      case DRV8834_MOTOR_B:
+        /* Break Motor B */
+        analogWrite(PIN_MB_ENABLE, LOW);
+        break;
+      case DRV8834_MOTOR_BOTH:
+        /* Break Both Motors */
+        analogWrite(PIN_MA_ENABLE, LOW);
+        analogWrite(PIN_MB_ENABLE, LOW);
+        break;
+      default:
+        /* Motor not recognized */
+        break;
+    }
 }
 
 /***************************************************************************************
@@ -88,7 +85,7 @@ void Motor_Break(void)
 void Motor_SwitchDirection(byte motorIdentifier, byte motorDirection)
 {
     /* To switch the rotating direction you must se the xPHASE accordingly
-     * Let's say that Forward is 1 while backwards is 0 */
+     * Forward is 1u while backwards is 0u */
 
     /* Ensure Direction is valid */
     if(LOW != motorDirection)
@@ -160,6 +157,40 @@ void Motor_EnableMotor(byte motorIdentifier, byte motorPower)
 }
 
 /***************************************************************************************
+ * Function: Motor_TestMotor()
+ ***************************************************************************************
+ * Description: This can be called to test a motor. The motor shall run for 1s, wait for
+ *              another 1s and then change direction.
+ * Parameters:
+ *  - motorIdentifier[in]   :   Identifier of the motor to be switched forward
+ *                              Supported Inputs: 
+ *                                  DRV8834_MOTOR_A == 1u
+ *                                  DRV8834_MOTOR_B == 2u
+ *                                  DRV8834_MOTOR_BOTH == 3u
+ **************************************************************************************/
+void Motor_TestMotor(byte motorIdentifier)
+{
+  /* To test the motor functionality, the motor shall run in each direction
+    and also the switching of the direction shall work. */
+
+  static byte direction = 0u;
+
+  /* Run the motor for 1s */
+  Motor_EnableMotor(motorIdentifier, 255u);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(DELAY_1_SECOND);
+
+  /* Stop the motor for 1s */
+  Motor_EnableMotor(motorIdentifier, 0u);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(DELAY_1_SECOND);
+
+  /* Switch Motor Direction */
+  Motor_SwitchDirection(motorIdentifier, !direction);
+  direction = !direction;
+}
+
+/***************************************************************************************
  * Function: setup()
  ***************************************************************************************
  * Description: This function will setup and initialize things at startup
@@ -167,12 +198,18 @@ void Motor_EnableMotor(byte motorIdentifier, byte motorPower)
 void setup(void)
 {
     /* PIN Modes */
+    pinMode(LED_BUILTIN, OUTPUT);
     pinMode(PIN_MA_ENABLE, OUTPUT);
     pinMode(PIN_MA_PHASE, OUTPUT);
     pinMode(PIN_MB_ENABLE, OUTPUT);
     pinMode(PIN_MB_PHASE, OUTPUT);
+    pinMode(PIN_DRV8834_SLEEP, OUTPUT);
 
-    /* Other */
+    /* Wakeup the Motor Driver */
+    digitalWrite(PIN_DRV8834_SLEEP, HIGH);
+
+    /* Set default direction of both Motors to move Forward */
+    Motor_SwitchDirection(DRV8834_MOTOR_BOTH, DRV8834_DIRECTION_FORWARD);
 }
 
 /***************************************************************************************
@@ -180,8 +217,8 @@ void setup(void)
  ***************************************************************************************
  * Description: This function is executed endlessly after the setup function
  **************************************************************************************/
-void loop() 
+void loop(void) 
 {
-    /* Do nothing for now */
-    delay(DELAY_DEFAULT);
+    /* Test the motors */
+    Motor_TestMotor(DRV8834_MOTOR_BOTH);
 }
