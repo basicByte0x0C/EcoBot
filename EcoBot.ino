83e99dfc312894f28b6498661a0b38d0783310cb
basicByte0x0C <>
2024-09-07 20:05:49 +0300

    Control phase: Add IR Manual controls

--- EcoBot.ino
+++ EcoBot.ino
@@ -2,6 +2,7 @@
  * Includes
  **************************************************************************************/
 #include "Notes.h"
+#include "IRremote.h"
 
 /***************************************************************************************
  * Macros
@@ -11,21 +12,49 @@
 #define E_NOT_OK        1u
 #define DELAY_1_SECOND  1000
 #define DELAY_DEFAULT   DELAY_1_SECOND
+#define SERIAL_BRATE    115200
 /* General Stuff end */
 
 /* Motor Stuff */
-#define PIN_MA_ENABLE       3
-#define PIN_MA_PHASE        4
-#define PIN_MB_ENABLE       5
-#define PIN_MB_PHASE        6
-#define PIN_DRV8834_SLEEP   7
+#define PIN_MA_ENABLE         3
+#define PIN_MA_PHASE          4
+#define PIN_MB_ENABLE         5
+#define PIN_MB_PHASE          6
+#define PIN_DRV8834_SLEEP     7
 
 #define DRV8834_MOTOR_A             1u
 #define DRV8834_MOTOR_B             2u
 #define DRV8834_MOTOR_BOTH          3u
 #define DRV8834_DIRECTION_BACKWARD  0u
 #define DRV8834_DIRECTION_FORWARD   1u
+#define DRV8834_POWER_FULL          255u
+#define DRV8834_POWER_HALF          128u
+#define DRV8834_POWER_NONE          0u
+#define DRV8834_WAKEUP_WAIT         1     /* Miliseconds until DRV8834 should be fully working after wakeup */
+#define DRV8834_WALK_TIME           100
+#define DRV8834_BREAK_TIMEOUT       110   /* Lower than this and the timeout is too short */
 /* Motor Stuff end */
+
+/* Exploration Stuff */
+#define EXPLORE_AUTOMATE  0u    /* Autonomous driving */
+#define EXPLORE_MANUAL    1u    /* Manual driving from IR */
+
+static byte exploreState = EXPLORE_AUTOMATE;
+/* Exploration Stuff end */
+
+/* IR Stuff */
+#define PIN_IR_RECEIVER_POWER   A0    /* Power the IR Receiver with this pin */
+#define PIN_IR_RECEIVER_DATA    9     /* Read data from IR Receiver with this pin */
+
+#define IR_VALUE_FORWARD        0xFF18E7    /* Move Backward */
+#define IR_VALUE_BACKWARD       0xFF4AB5    /* Move Forward */
+#define IR_VALUE_LEFT           0xFF10EF    /* Rotate Left */
+#define IR_VALUE_RIGHT          0xFF5AA5    /* Rotate Right */
+#define IR_VALUE_MODE           0xFF38C7    /* Switch between Manual and Automate Exploring */
+
+IRrecv irrecv(PIN_IR_RECEIVER_DATA);
+decode_results results;
+/* IR Stuff end */
 
 /***************************************************************************************
  * Function: Motor_Break()
@@ -173,7 +202,7 @@ void Motor_TestMotor(byte motorIdentifier)
   /* To test the motor functionality, the motor shall run in each direction
     and also the switching of the direction shall work. */
 
-  static byte direction = 0u;
+  static byte direction = 1u;
 
   /* Run the motor for 1s */
   Motor_EnableMotor(motorIdentifier, 255u);
@@ -191,12 +220,142 @@ void Motor_TestMotor(byte motorIdentifier)
 }
 
 /***************************************************************************************
+ * Function: HandleIR()
+ ***************************************************************************************
+ * Description: This function checks the IR readings and do something about it.
+ **************************************************************************************/
+void HandleIR(void)
+{
+    /* Debug Stuff */
+    Serial.print("HandleIR::I Received IR: ");
+    Serial.println(results.value, HEX);
+
+    switch(results.value)
+    {
+        case IR_VALUE_FORWARD: 
+            /* Move Forward */
+            Motor_SwitchDirection(DRV8834_MOTOR_BOTH, DRV8834_DIRECTION_FORWARD);
+            Motor_EnableMotor(DRV8834_MOTOR_BOTH, DRV8834_POWER_FULL);
+            delay(DRV8834_WALK_TIME);
+            break;
+        case IR_VALUE_BACKWARD:
+            /* Move Backwards */
+            Motor_SwitchDirection(DRV8834_MOTOR_BOTH, DRV8834_DIRECTION_BACKWARD);
+            Motor_EnableMotor(DRV8834_MOTOR_BOTH, DRV8834_POWER_FULL);
+            delay(DRV8834_WALK_TIME);
+            break;
+        case IR_VALUE_LEFT:
+            /* Rotate Left */
+            Motor_SwitchDirection(DRV8834_MOTOR_A, DRV8834_DIRECTION_BACKWARD);
+            Motor_SwitchDirection(DRV8834_MOTOR_B, DRV8834_DIRECTION_FORWARD);
+            Motor_EnableMotor(DRV8834_MOTOR_BOTH, DRV8834_POWER_FULL);
+            delay(DRV8834_WALK_TIME);
+            break;
+        case IR_VALUE_RIGHT:
+            /* Rotate Right */
+            Motor_SwitchDirection(DRV8834_MOTOR_A, DRV8834_DIRECTION_FORWARD);
+            Motor_SwitchDirection(DRV8834_MOTOR_B, DRV8834_DIRECTION_BACKWARD);
+            Motor_EnableMotor(DRV8834_MOTOR_BOTH, DRV8834_POWER_FULL);
+            delay(DRV8834_WALK_TIME);
+            break;
+        case IR_VALUE_MODE:
+            /* Change Explore State */
+            exploreState = !exploreState;
+            break;
+        default:
+            /* Do nothing */
+            break;
+    }
+}
+
+/***************************************************************************************
+ * Function: CheckForCommands()
+ ***************************************************************************************
+ * Description: This function verify if there are custom commands sent through IR.
+ **************************************************************************************/
+void CheckForCommands(void)
+{
+    static volatile long breakTime = 0;
+
+    /* Explore Stuff */
+    if(exploreState == EXPLORE_AUTOMATE)
+    {
+        /* Try to read IR Receiver */
+        if(irrecv.decode(&results))
+        {
+            /* Debug Stuff */
+            Serial.print("CheckForCommands::I received IR: ");
+            Serial.println(results.value, HEX);
+
+            /* Resume IR */
+            irrecv.resume();
+
+            /* Check for Explore Change */
+            if(IR_VALUE_MODE == results.value)
+            {
+                /* Change Explore State */
+                exploreState = !exploreState;
+            }
+            else
+            {
+                /* Do Nothing */
+            }
+        }
+        else
+        {
+            /* Do Autonomous things */
+            /* Check for Obstacles */
+            /* Decide next Direction */
+            /* Move */
+        }
+    }
+    else
+    {
+        /* Do Manual things */
+        /* Check IR Input */
+        if(irrecv.decode(&results))
+        {
+            /* Resume IR */
+            irrecv.resume();
+
+            /* Handle IR Input */
+            HandleIR();
+
+            /* Start Break timeout */
+            breakTime = millis();
+        }
+        else
+        {
+            /* Debug Stuff */
+            //Serial.println("CheckForCommands::I received nothing");
+
+            /* Stop motors if some time passed since last command */
+            if((0 != breakTime) && (DRV8834_BREAK_TIMEOUT < (millis() - breakTime)))
+            {
+                /* Stop Motors */
+                Motor_BreakMotor(DRV8834_MOTOR_BOTH);
+
+                /* Disable Break Timeout */
+                breakTime = 0;
+            }
+            else
+            {
+                /* Do Nothing */
+            }
+        }
+    }
+}
+
+/***************************************************************************************
  * Function: setup()
  ***************************************************************************************
  * Description: This function will setup and initialize things at startup
  **************************************************************************************/
 void setup(void)
 {
+    /* Debug Stuff */
+    Serial.begin(SERIAL_BRATE);
+
     /* PIN Modes */
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(PIN_MA_ENABLE, OUTPUT);
@@ -204,12 +363,21 @@ void setup(void)
     pinMode(PIN_MB_ENABLE, OUTPUT);
     pinMode(PIN_MB_PHASE, OUTPUT);
     pinMode(PIN_DRV8834_SLEEP, OUTPUT);
+    pinMode(PIN_IR_RECEIVER_POWER, OUTPUT);
+    pinMode(PIN_IR_RECEIVER_DATA, INPUT);
 
     /* Wakeup the Motor Driver */
     digitalWrite(PIN_DRV8834_SLEEP, HIGH);
+    delay(DRV8834_WAKEUP_WAIT);
 
     /* Set default direction of both Motors to move Forward */
     Motor_SwitchDirection(DRV8834_MOTOR_BOTH, DRV8834_DIRECTION_FORWARD);
+
+    /* Power on the IR Receiver */
+    digitalWrite(PIN_IR_RECEIVER_POWER, HIGH);
+
+    /* Enable IR Receiver */
+    irrecv.enableIRIn();
 }
 
 /***************************************************************************************
@@ -219,6 +387,9 @@ void setup(void)
  **************************************************************************************/
 void loop(void) 
 {
-    /* Test the motors */
-    Motor_TestMotor(DRV8834_MOTOR_BOTH);
+    /* Battery Management */
+    //TODO();
+
+    /* Movement Control */
+    CheckForCommands();
 }
--- Notes.h
+++ Notes.h
@@ -1,3 +1,5 @@
+#ifndef NOTES_H
+#define NOTES_H
 /***************************************************************************************
  * Notes
  **************************************************************************************/
@@ -7,7 +9,9 @@
 /* BMS :: Will use any 1S with as much protections as possible */
 /* SOLAR PANEL :: Will use one 6V Panel that fits the robot chasis. */
 /* SOLAR CHARGER :: Will use one simple CN3065 Solar Charger */
-/* SENSOR :: Will use one HC-SR04 sensor to be aware of the surroundings */
+/* SENSORS :: IR Receiver for Remote Control 
+            IR Distance for Obstacle Avoidance 
+            Laser ToF for stairs avoidance(it won't fall down the stairs) */
 
 /* ----- Arduino Pro Mini -----
  * - Builtin LED will be removed to reduce Energy Consumption, but after everything works(will be used for debug for now).
@@ -22,13 +26,13 @@
  *  D5  --- Used by Motor B Enable
  *  D6  --- Used by Motor B Phase
  *  D7  --- Used by DRV8834 Sleep Pin
- *  D8  --- (Future) Used by HC-SR04 Trig Pin
- *  D9  --- (Future) Used by HC-SR04 Echo Pin
+ *  D8  --- 
+ *  D9  --- Used to read IR Receiver
  *  D10 --- Reserved for SPI SS(CS)
  *  D11 --- Reserved for SPI MOSI
  *  D12 --- Reserved for SPI MISO
  *  D13 --- Reserved for SPI SCK
- *  A0  --- 
+ *  A0  --- Used to power IR Receiver
  *  A1  ---
  *  A2  ---
  *  A3  ---
@@ -43,6 +47,7 @@
  * - CFG pin is wired to GND(maybe not needed?).
  * - M1 pin is wired to GND(we don't really need to use it, and Slow Decay is perfect).
  * - Motors need some amperage, doesn't work powered by TTL Serial Programmer, must use a LiPo Battery or similar.
+ * - In documentation it is mentioned that when waking up from Sleep the Driver might take up to 1ms to become functional.
  */
 
 /* ----- LiPo Battery -----
@@ -51,6 +56,31 @@
  * - For BMS one small, round, green, unnamed, 1S component was used that is based on 8205A Z1J0802 chip.
  * - For Solar Controller an CN3065 Solar Charger v1.0 was used, MPPT doesn't really make sense vor a 6V Panel.
  * - LiPo connected to BMS -> BMS connected to Solar Charger -> Solar Charger connected to Solar Panel + Robot(Arduino+DRV8834).
+ */
+
+/* ----- IR Remote Control -----
+ * - To remote controll the Robot i added a IR Receiver
+ * - IR Data is handled on one pin and another pin is used to power the IR Receiver.
+ * - IR Data Values are defined, such it can be changed to any value
+ * - IR Receiver i used is an unknown receiver with 3 pins: VCC, GND and Source. I got it from a Plusivo Starter Kit.
+ * - Library used to controll the receiver is from the same Plusive Starter Kit examples.
+ * - Robot will listen for a IR Mode Change, when this value is received from IR then the Explore State is changed between 
+ *      Autonomous and Manual.
+ * - In Manual State, it will listen for IR Commands and execute them.
+ * - In Autonomous State it will walk autonomously and avoid obstacles with sensors.
+ * - If IR is not resumed after reading it it will be stuck with the same value forever. Resume let it read the next command.
+ */
+
+/* ----- IR Distance Sensor ----- 
+ * - It will be used to detect objects in the path of the robots.
+ * - If an object is detected, the robot will rotate around and try to find another path with no obstacles.
+ */
+
+/* ----- Laser ToF Sensor -----
+ * - It will be used to detect stairs going down.
+ * - It will be positioned looking down ahead, probably at an angle of 45 or 60 degrees.
+ * - If a the measured distance is higher than a configured threshold then this means there is a hole/stairs ahead
+ *      => don't move 
  */
 
 /* TODO: Function to set Driver Max Current 
@@ -75,3 +105,4 @@
  * - Motor Driver put into sleep mode by setting the SLEEP Pin to LOW.
  * - Arduino will sleep and wake up from time to time to check Battery level.
  * */
+#endif /* NOTES_H */
